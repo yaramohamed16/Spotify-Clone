dc8e6494dc077dc04b1cf42ec7f5debfaa4d6b77
Yara <yarayassin1611@gmail.com>
2024-02-01 00:12:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//App
+import React, { useEffect } from "react";
+import Login from "./components/Login";
+import Spotify from "./components/Spotify";
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
+export default function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if (token) {
+        dispatch({ type: reducerCases.SET_TOKEN, token });
+      }
+    }
+    document.title = "Spotify";
+  }, [dispatch, token]);
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initialState } from "./utils/reducer";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// index.js
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import { StateProvider } from "./utils/StateProvider";
+// import reducer, { initialState } from "./utils/reducer";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   <React.StrictMode>
+//     <StateProvider initialState={initialState} reducer={reducer}>
+//       <App />
+//     </StateProvider>
+//   </React.StrictMode>
+// );
+
+// // Check if service worker is supported
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("/service-worker.js") // Use the correct path to your service worker file
+//     .then((registration) => {
+//       console.log("Service Worker registered with scope:", registration.scope);
+//     })
+//     .catch((error) => {
+//       console.error("Error registering Service Worker:", error);
+//     });
+// }
