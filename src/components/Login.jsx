0a0d7e5184b3d1c2eab211e9446d8ea534c215f1
Yara <yarayassin1611@gmail.com>
2024-02-01 22:25:10 +0200

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,11 @@
-//login:
-import React from "react";
+// login:
+import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 export default function Login() {
+  const [loginError, setLoginError] = useState(false);
+
   const handleClick = async () => {
     const client_id = "361aee247b75456d930bdfbc6c29b807";
     const redirect_uri = "https://spotify-clone16.netlify.app/";
@@ -16,17 +19,38 @@ export default function Login() {
       "user-read-recently-played",
       "user-top-read",
     ];
-    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
-      " "
-    )}&response_type=token&show_dialog=true`;
+
+    try {
+      window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+        " "
+      )}&response_type=token&show_dialog=true`;
+    } catch (error) {
+      // Check if the error is due to user not being registered
+      if (axios.isAxiosError(error) && error.response && error.response.status === 403) {
+        const errorMessage = error.response.data.error.message;
+        if (errorMessage.includes("User not registered in the Developer Dashboard")) {
+          // Handle the case where the user is not registered
+          setLoginError(true);
+        } else {
+          console.error("Unexpected error during login:", error);
+        }
+      } else {
+        console.error("Unexpected error during login:", error);
+      }
+    }
   };
+
   return (
     <Container>
       <img
         src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
         alt="spotify"
       />
-      <button onClick={handleClick}>Connect Spotify</button>
+      {loginError ? (
+        <p>Error: User not registered in the Developer Dashboard</p>
+      ) : (
+        <button onClick={handleClick}>Connect Spotify</button>
+      )}
     </Container>
   );
 }
@@ -51,5 +75,9 @@ const Container = styled.div`
     border: none;
     font-size: 1.4rem;
     cursor: pointer;
+  }
+  p {
+    color: red;
+    font-size: 1.4rem;
   }
 `;
